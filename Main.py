44c8a726c2815d8a170a34b993651f21ab0b175f
n0miya <dulapah6@gmail.com>
2021-10-26 10:08:00 +0700

    Update MainMenu.psd

--- Main.py
+++ Main.py
@@ -684,9 +684,15 @@ class AceMath(Tk):
         hideWidgetList = [self.diag_box, self.pre_countdown, self.user_answer, self.rand_int_text, self.account_prompt, self.cancel_game, self.cancel_game_yes, self.cancel_game_no]
         for widget in hideWidgetList:
             self.hide_widget(widget)
+        self.write_data("selectedDifficulty", "null")
         self.write_data("isUserInGame", "False")
         self.write_data("isStopwatchPaused", "False")
         self.write_data("isGameStarted", "False")
+        self.write_data("currentQuestionNumber", 0)
+        self.write_data("answer", 0)
+        self.write_data("minInteger", 0)
+        self.write_data("maxInteger", 0)
+        self.write_data("questionSize", 0)
         winsound.PlaySound(data + "sounds/BGMusic.wav", winsound.SND_ALIAS | winsound.SND_ASYNC | winsound.SND_LOOP)
         self.to_main_menu()
 
